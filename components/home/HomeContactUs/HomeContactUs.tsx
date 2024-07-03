966f62f5f0301080fd188a10f1f098fc9097d8bd
slv-dvishwakarma <dvishwakarma@skilline.ai>
2024-07-03 19:28:35 +0530

    all done site goes to live

--- components/home/HomeContactUs/HomeContactUs.tsx
+++ components/home/HomeContactUs/HomeContactUs.tsx
@@ -7,16 +7,17 @@ export const HomeContactUs = () => {
   return (
     <div className="container md:pt-5 pb-[50px]">
       <Heading className="text-center">Contact us</Heading>
-      <div className="grid md:grid-cols-3 grid-cols-1 md:gap-10 items-center">
+      <div className="grid md:grid-cols-2 grid-cols-1 md:gap-5">
+        <div className="xl:space-y-0 lg:space-y-0 md:space-y-0 space-y-3">
         <ContactIconBox title="Phone Number" icon="phone">
           (+91) xxxx xxx xxx
         </ContactIconBox>
-        <ContactIconBox title="Our Location" icon="map">
-          594, Road No. 34, Survey Of India Ayyappa Society, Ayyappa Society, Chanda Naik Nagar, Madhapur, Hyderabad, Telangana 500081
-        </ContactIconBox>
-
         <ContactIconBox title="Email Address" icon="email">
           <Link href="mailto:info@skilline.ai">info@skilline.ai</Link>
+        </ContactIconBox>
+        </div>
+        <ContactIconBox title="Our Location" icon="map">
+          594, Road No. 34, Survey Of India Ayyappa Society, Ayyappa Society, Chanda Naik Nagar, Madhapur, Hyderabad, Telangana 500081
         </ContactIconBox>
       </div>
     </div>
